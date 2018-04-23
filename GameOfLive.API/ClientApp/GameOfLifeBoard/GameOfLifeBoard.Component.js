24ade0c0c8203dc199b22a6f67eb816a7a84ab8a
Ever Sierra <esierra@tiempodev.com>
2018-04-23 12:43:49 -0500

    Add row and colum size validation

--- GameOfLive.API/ClientApp/GameOfLifeBoard/GameOfLifeBoard.Component.js
+++ GameOfLive.API/ClientApp/GameOfLifeBoard/GameOfLifeBoard.Component.js
@@ -4,18 +4,34 @@
         controller: ['$route','$routeParams','$location','GameOfLifeService', function ($route, $routeParams,$location, gameOfLifeService) {
 
             var self = this;
-            //self.board = [[1, 1, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]];
             self.rowCount = $routeParams.rowCount ? $routeParams.rowCount : 10;
             self.columnCount = $routeParams.columnCount ? $routeParams.columnCount : 10;
+            self.randomStates = $routeParams.randomStates ? $routeParams.randomStates : false;
+
+            var sizeErrorMessage = "The maximum number of colums or rows allowed is 50";
+
+            if (self.columnCount > 50) {
+                self.columnCount = 50;
+                self.error = sizeErrorMessage;
+            }
+
+            if (self.rowCount > 50) {
+                self.rowCount = 50;
+                self.error = sizeErrorMessage;
+            }
            
             var onError = function (reason) {
                 self.error = 'Could not perform step';
             }
 
             var onStepPerformed = function (data) {
-
                 self.board = data;
+            }
 
+            var createCellState = function () {
+
+                if (self.randomStates) return Math.floor(Math.random() * 2);
+                else return 0;
             }
 
             var setBoardSize = function (rowCount,columnCount) {
@@ -23,30 +39,28 @@
 
                 for (var i = 0; i < rowCount; i++) {
                     var row = new Array(columnCount);
-                    for (var j = 0; j < columnCount; j++) row[j] = 0;
+                    for (var j = 0; j < columnCount; j++) row[j] = createCellState();
                     rows[i] = row;
                 }
 
                 self.board = rows;
             }
 
+
             self.changeBoardSize = function changeBoardSize() {
-                //$routeParams.rowCount = self.rowCount;
-                //$routeParams.columnCount = self.columnCount;
-     
-                $route.updateParams({ rowCount: self.rowCount, columnCount: self.columnCount });
+                $route.updateParams({ rowCount: self.rowCount, columnCount: self.columnCount, randomStates: self.randomStates });
+            }
+
+            self.reset = function reset() {
+                $route.reload();
             }
 
             self.changeCell = function changeCell(rowIndex, columnIndex) {
-
                 self.board[rowIndex][columnIndex] = self.board[rowIndex][columnIndex] == 0 ? 1 : 0;  
-               
             }
 
             self.performStep = function(){
-
                 gameOfLifeService.performStep(self.board).then(onStepPerformed,onError);
-
             }
 
             setBoardSize(self.rowCount, self.columnCount);
